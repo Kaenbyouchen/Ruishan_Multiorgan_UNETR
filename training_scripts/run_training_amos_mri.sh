3d9f2cfc015ec150e2048e00439ac06c5477db49
Rui <weisu@RuideMacBook-Air.local>
2026-03-24 16:06:10 -0700

    adding amos mri

--- training_scripts/run_training_amos_mri.sh
+++ training_scripts/run_training_amos_mri.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 PROJ=/project2/ruishanl_1185/zrui6736/Ruishan_Multiorgan_UNETR_new
-DATASET_PATH=/project2/ruishanl_1185/zrui6736/Ruishan_Multiorgan_UNETR_new/DATASET_AMOSMRI
+DATASET_PATH=$PROJ/DATASET_AMOSMRI
 
-export PYTHONPATH=$PROJ/unetr_plus_plus-main
-export RESULTS_FOLDER=$PROJ/unetr_plus_plus-main/unetr_pp/results/output_amos_mri
-export unetr_pp_raw_data_base="$DATASET_PATH"/unetr_pp_raw
-export unetr_pp_preprocessed="$DATASET_PATH"/unetr_pp_preprocessed
+export PYTHONPATH=$PROJ
+export RESULTS_FOLDER=$PROJ/unetr_pp/results/output_amos_mri
+export unetr_pp_raw_data_base=$DATASET_PATH/unetr_pp_raw
+export unetr_pp_preprocessed=$DATASET_PATH/unetr_pp_preprocessed
 
-python $PROJ/unetr_plus_plus-main/unetr_pp/run/run_training.py 3d_fullres unetr_pp_trainer_amos_mri 7 0
+python $PROJ/unetr_pp/run/run_training.py 3d_fullres unetr_pp_trainer_amos_mri 7 0
