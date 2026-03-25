74e049c612245156d5db921ea93ab6b97e512d52
Kaenbyouchen <18310236898@163.com>
2026-03-25 13:19:02 -0700

    adding amos mri evaluation

--- evaluation_scripts/run_evaluation_amos_mri.sh
+++ evaluation_scripts/run_evaluation_amos_mri.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-DATASET_PATH=../DATASET_AMOSMRI
-CHECKPOINT_PATH=../output_amos_mri
+PROJ=/project2/ruishanl_1185/zrui6736/Ruishan_Multiorgan_UNETR_new
+DATASET_PATH=$PROJ/DATASET_AMOSMRI
 
-export PYTHONPATH=.././
-export RESULTS_FOLDER="$CHECKPOINT_PATH"
-export unetr_pp_preprocessed="$DATASET_PATH"/unetr_pp_preprocessed
-export unetr_pp_raw_data_base="$DATASET_PATH"/unetr_pp_raw
+export PYTHONPATH=$PROJ
+export RESULTS_FOLDER=$PROJ/unetr_pp/results/output_amos_mri
+export unetr_pp_raw_data_base=$DATASET_PATH/unetr_pp_raw
+export unetr_pp_preprocessed=$DATASET_PATH/unetr_pp_preprocessed
 
-python ../unetr_pp/run/run_training.py 3d_fullres unetr_pp_trainer_amos_mri 7 0 -val
+python $PROJ/unetr_pp/run/run_training.py 3d_fullres unetr_pp_trainer_amos_mri 7 0 -val
